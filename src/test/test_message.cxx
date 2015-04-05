d9960cd5cbe9406aefadacde114d968af64e053c
Bartłomiej Kurek <minus@noise-nation.net>
2015-04-06 00:10:16 +0300

    VERY Basic message test. Added basic test util.

--- src/test/test_message.cxx
+++ src/test/test_message.cxx
@@ -1,10 +1,43 @@
-#include <nix/common.hxx>
-//#include <nix/message.hxx>
-
-INITIALIZE_EASYLOGGINGPP
+#include "base.hxx"
+#include <nix/message.hxx>
+#include <string>
+#include <iostream>
 
 int main()
 {
+	using nix::Message;
+	using std::string;
 
-	return 0;
+	UnitTest unit;
+
+	unit.test_case(
+		"Basic", 
+		[](TestCase& test)
+		{
+			Message m;
+			std::string expected("{}\n");
+			test.equals(m.to_string(), expected, "is empty");
+			
+			m.set("string", "value");
+			m.set("int", 123);
+			expected = "{\"int\":123,\"string\":\"value\"}\n";
+			test.equals(m.to_string(), expected, "to_string()");
+		}
+	);
+
+	unit.test_case(
+		"Copy message", 
+		[](TestCase& test)
+		{
+			Message m1;
+			m1.set("string", "value");
+			Message m2 = m1;
+			
+			std::string excected = "{\"string\":\"value\"}\n";
+			test.equals(m2.to_string(), excected, "copied message to_string()");
+		}
+	);
+
+
+	return unit.run();
 }
